8d015cbb4943f15bf603ff4be06de68ab7ff82d9
ecumene-software <mitchell.hynes@ecumene.xyz>
2016-11-07 22:18:10 -0330

    LibGDX Commit

---
+++ silicon-tycoon/desktop/src/ecu/silicon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ecu.silicon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ecu.silicon.SiliconTycoon;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SiliconTycoon(), config);
+	}
+}
